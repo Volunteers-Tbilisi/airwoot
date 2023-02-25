90bbe70c79ad70bb501ff8cf805b633445fb52e8
Slava Zakharov <slava.zakharov@mailbox.org>
2023-02-25 16:06:42 +0300

    Initial commit

--- netlify/functions/helpers/airtable.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-import Airtable from 'airtable';
-
-dotenv.config();
-
-const base = new Airtable({ apiKey: process.env.AIRTABLE_TOKEN }).base(process.env.AIRTABLE_BASE);
-const setBase = (TABLE_REF) => base(TABLE_REF);
-
-export default setBase;
--- netlify/functions/helpers/formattedReturn.js
+++
@@ -1,9 +0,0 @@
-export default (statusCode, body) => {
-	return {
-		statusCode,
-		body: JSON.stringify(body),
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-		},
-	};
-};
